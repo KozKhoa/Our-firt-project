7e811d31347a696b232191b6cfc551ee0f249b2e
KozKhoa <khoahuynh787898@gmail.com>
2024-05-02 22:36:52 +0700

    Fix the endline

--- Demo.cpp
+++ Demo.cpp
@@ -5,6 +5,6 @@ int main()
     int n = 0;
     printf("Hello world: ");
     std::cin >> n;
-    std::cout << "You enter: " << n << '\n;
+    std::cout << "You enter: " << n << '\n';
     return 0;
 }
